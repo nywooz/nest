a2ed19cb632c3886b74ad01a26722a018a6047c2
Noor <noor@bulbstudios.com>
2020-07-18 23:16:37 +0100

    added base files

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,33 +1,4 @@
-import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
-import { CoursesService } from './courses.service';
-import { CreateCourseDto } from './create-course.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {
-    constructor(private coursesService: CoursesService) {}
-
-    @Get()
-    async getCourses() {
-        const courses = await this.coursesService.getCourses();
-        return courses;
-    }
-
-    @Get(':courseId')
-    async getCourse(@Param('courseId') courseId) {
-        const course = await this.coursesService.getCourse(courseId);
-        return course;
-    }
-
-    @Post()
-    async addCourse(@Body() createCourseDto: CreateCourseDto) {
-        const course = await this.coursesService.addCourse(createCourseDto);
-        return course;
-    }
-
-    @Delete()
-    async deleteCourse(@Query() query) {
-        const courses = await this.coursesService.deleteCourse(query.courseId);
-        return courses;
-    }
-}
-
+export class CoursesController {}
--- src/courses/create-course.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateCourseDto {
-    readonly id: number;
-    readonly title: string;
-    readonly description: string;
-    readonly author: string;
-    readonly url: string;
-}
